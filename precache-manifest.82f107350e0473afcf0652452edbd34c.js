d8fd08a5ed4ae8af5560b2bd385fd57e1dcf5c38
andrearose26 <andreaa.lacson@gmail.com>
2019-11-28 10:51:31 -0500

    Updates

---
+++ precache-manifest.82f107350e0473afcf0652452edbd34c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7ce8fd14e8fdc2fecd9932d12bdcf5",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "332dd5afdbfd91cc0443",
+    "url": "/react-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6fb0c9ca5cbd80fe2a91",
+    "url": "/react-gh-pages/static/js/2.e1806175.chunk.js"
+  },
+  {
+    "revision": "332dd5afdbfd91cc0443",
+    "url": "/react-gh-pages/static/js/main.57855b05.chunk.js"
+  },
+  {
+    "revision": "3703c47d598af9a7e5b4",
+    "url": "/react-gh-pages/static/js/runtime-main.ead2228f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
